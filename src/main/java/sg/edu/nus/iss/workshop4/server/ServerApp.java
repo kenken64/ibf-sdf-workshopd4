7e93f57366ae5dc526fe5b41e3674a7d3d41464c
kenken64 <bunnyppl@hotmail.com>
2024-02-07 14:37:53 +0800

    upd

--- src/main/java/sg/edu/nus/iss/workshop4/server/ServerApp.java
+++ src/main/java/sg/edu/nus/iss/workshop4/server/ServerApp.java
@@ -10,7 +10,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerApp {
-    @SuppressWarnings("resource")
+    
     public static void main(String[] args) {
         System.out.println("ServerApp.main()");
         Socket socket = null;
