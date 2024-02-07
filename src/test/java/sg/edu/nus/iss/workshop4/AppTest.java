4c3137ca1431ef649b30a74992010e693998cdf6
kenken64 <bunnyppl@hotmail.com>
2024-02-07 14:22:18 +0800

    upd

--- src/test/java/sg/edu/nus/iss/workshop4/AppTest.java
+++ src/test/java/sg/edu/nus/iss/workshop4/AppTest.java
@@ -1,8 +1,12 @@
 package sg.edu.nus.iss.workshop4;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import sg.edu.nus.iss.workshop4.server.Cookie;
 
 /**
  * Unit test for simple App.
@@ -16,5 +20,14 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testGetRandomCookieName()
+    {
+        String cookieName = Cookie
+            .getRandomCookie("/home/kenneth/Projects/NUS-ISS-SDF/sdf-workshopd4/cookie_file.txt");
+        System.out.println(cookieName);
+        assertNotNull( cookieName );
     }
 }
